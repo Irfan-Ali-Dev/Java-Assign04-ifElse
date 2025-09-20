ae6b5a434af35f6555d5ae19d6bca4f5faa2168b
Irfan-Ali-Dev <yousafbuilders.it@gmail.com>
2025-09-21 02:49:57 +0500

    30 Answer Updated

---
+++ 01/app.js
@@ -0,0 +1,113 @@
+// Ans: 01
+// var age = prompt( 18)
+// if (age <= 17){
+//     console.log = alert("You are a minor");
+// }else {
+//     console.log = alert ("You are adult");    
+// }
+
+// Ans: 02
+// var number = prompt("Enter Your Number")
+// if(Number (number)>=0){
+//     console.log("Positive number");    
+// }else {
+//     console.log("Not Positive");    
+// }
+
+// // Ans: 03
+// var number = prompt("Enter Your Number")
+// if (number % 2===0){
+//     console.log("Even Number");    
+// }else {
+//     console.log("Odd Number");    
+// }
+
+// Ans: 04
+// var userName = prompt ("Enter User Name")
+// if (userName === "admin"){console.log("Welcome, Admin");
+// }else { console.log("Access denied");}
+
+// Ans: 05
+// var num1 = prompt("Enter your 1st number")
+// var num2 = prompt("Enter your 2nd Number")
+// if(num1===num2){console.log("Both number are the same");
+// }else {console.log("The numbers are different");}
+
+// Ans: 06
+// var num1 = prompt("Enter your 1st number")
+// var num2 = prompt("Enter your 2nd Number")
+// if(num1>=num2){console.log("First is Lager");
+// }else if (num2>=num1) {console.log("Second is Lager");
+// }else if (num1===num2) {console.log("Both numbers are equal");
+// }else {console.log("Whats that!");}
+
+// Ans: 07
+// var number = prompt("Enter a Number")
+// if (number <=100){console.log("Within Range");
+// }else {console.log("Too Big!");}
+
+// Ans: 08
+// var score = prompt("Enter Your Score")
+// if (score >=50){console.log("You Passed!");
+// }else {console.log("You Failed!");}
+
+// Ans: 09
+// var temperature = prompt("Enter Temperature")
+// if (temperature >=30){console.log("It's Hot!");
+// }else {console.log("It's not Hot!");}
+
+// Ans: 10
+// var number = prompt("Enter Number");
+// if (Number(number) === 0){alert("Zero");
+// }else {alert("It's not Zero!");}
+
+// Ans: 11
+// var age = prompt("Enter you Age")
+// if (age >=60){console.log("You are a senior citizen.");
+// }else {console.log("You are not a senior.");}
+
+// Ans: 12
+// var userName = prompt("Enter you Name")
+// if (userName === "Alice"){alert("Hi, Alice");
+// }else {alert("You are not Alice");}
+
+// Ans: 13
+// var password = prompt("Enter you Password")
+// if (password === "Ali123"){alert("Correct Password");
+// }else {alert("Wrong Password");}
+
+// Ans: 14
+// var hour = prompt("Enter a Hour")
+// if (hour <= 12){alert("Good Morning");
+// }else {alert("Good Afternoon");}
+
+// Ans: 15
+// var number = prompt("Enter a number")
+// if (Number (number) % 5===0){console.log("Divisible by 5");
+// }else {console.log("Not Divisible by 5");}
+
+// Ans: 16
+// var game = prompt("Type (Yes / No ) to play game")
+// if (game === "Yes" || game==="yes"){alert("Let's Play");
+// }else {alert("Maybe next time");}
+
+// Ans: 17
+// var color = prompt("Chose a Color")
+// if (color === "Blue" || color === "blue" ){console.log("Cool choice!");
+// }else {console.log("Interesting color!");}
+
+// Ans: 18
+// var week = prompt("Enter today Weeday")
+// if (week === "Sunday" || week === "sunday"){alert("Weekend");
+// }else {alert("Weekday");}
+
+// Ans: 19
+// var fruit = prompt("Enter Fruit Name")
+// if (fruit === "Banana" || fruit === "banana" || fruit === "Apple" || fruit === "apple"){alert("We have that!");
+// }else {alert("Sorry, out of stock.");}
+
+// Ans: 20
+var game = prompt("Type Start to Begin")
+if (game === "Start" || game === "start"){alert("Game started!");
+}else {alert("Waiting for input...");}
+
---
+++ 02/app.js
@@ -0,0 +1,95 @@
+// Ans: 01
+// var speed = 50;
+// if(speed <= 30){
+//     console.log("Too slow");  
+// } else if(speed > 30 && speed <= 60){
+//     console.log("Safe speeding");
+// } else if(speed > 60 && speed <= 100){
+//     console.log("Speeding");
+// } else {
+//     console.log("Dangerously Fast");
+// }
+
+// Ans: 02
+// var returnBook = 15
+// if(returnBook === 0){
+//     console.log("Koi fine nahi");
+// } else if(returnBook >= 1 && returnBook <= 5){
+//     console.log("$1 ka fine lagaiga ==> $" , returnBook * 1);
+// } else if(returnBook >= 6 && returnBook <= 10){
+//     console.log("$2 ka fine lagaiga ==> $" , returnBook * 2);
+// } else {
+//     console.log("$5 ka fine lagaiga ==> $" , returnBook * 5);
+// }
+
+// Ans: 03
+// var ticket = true;
+// var age = 12;
+// if(ticket && age >= 18){
+//     console.log("Welcome to our event");   
+// } else {
+//     console.log("Hooo kon bhai tum! kisna bulaya ha");
+// }
+
+// Ans: 04
+// var battery = -2;
+// if(battery >= 80 && battery <= 100){
+//     console.log("Fully charge");
+// } else if(battery >= 50 && battery <= 79){
+//     console.log("Good Battery");
+// } else if(battery >= 20 && battery <= 49){
+//     console.log("low battery");
+// } else if(battery <= 20 && battery >= 0){
+//     console.log("Charge krla bhai!");
+// } else {
+//     console.log("kiya chah rha ha");
+// }
+
+// Ans: 05
+// var customerType = "student";
+// var age = 20;
+// if(customerType === "senior" && age >= 60){
+//     console.log("30% ka discount miljayega");
+// } else if(customerType === "student" && age >= 20){
+//     console.log("Bacha ha tu 20% discount lela");
+// } else {
+//     console.log("lena ha to loo warna aga brho.");
+// }
+
+// Ans: 06
+// var temperature = -10;
+// if(temperature <= 10){
+//     console.log("Heavy jacket");
+// } else if(temperature > 10 && temperature <= 20){
+//     console.log("sweater");
+// } else if(temperature > 20 && temperature <= 30){
+//     console.log("Tshirt");
+// } else {
+//     console.log("chaddi banyaan");
+// }
+
+// Ans: 07
+// var water = 9
+// if (water >= 2 && water <=2.4){ console.log("Sedentary");
+// }else if(water >= 2.5 && water <= 2.9) {console.log("Moderate");
+// }else if(water >= 3 && water <= 10) {console.log("Active");
+// }else {console.log("Whats That");} 
+
+// Ans: 08
+// var mark1 = 40
+// var mark2 = 40
+// var mark3 = 66
+// if (mark1 >= 40 && mark2 >= 40 && mark3 >= 40) {console.log("Pass");
+// }else {console.log("Fail");}
+
+// Ans: 09
+// var theme = 170
+// if (theme >= 6 && theme <= 17) {console.log("Light Mode");
+// }else {console.log("Dark Mode");}
+
+Ans: 10
+var time = 18
+if (time >= 6 && time <= 10){ console.log("Breakfast");
+}else if(time >= 12 && time <= 14) {console.log("Lunch");
+}else if(time >= 18 && time <= 21) {console.log("Dinner");
+}else {console.log("Snack");} 
